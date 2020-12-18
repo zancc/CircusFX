75aae2dfb306b1a93b4c01f8d1094036b6345868
Zane Kaibe <zane.kaibe@gmail.com>
2020-12-18 17:32:23 +0200

    First commit

---
+++ src/module-info.java
@@ -0,0 +1,6 @@
+module CircusFX {
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    opens sample;
+}
---
+++ src/sample/Main.java
@@ -0,0 +1,30 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Circus Board Game");
+        primaryStage.setScene(new Scene(root, 1200, 650));
+        primaryStage.show();
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
